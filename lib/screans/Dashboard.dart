18945e1342959cee615b3a9868fe7ebbf2555d6f
abhishek peiris <abhishekdananjaya1@yahoo.com>
2022-10-13 14:49:17 +0530

    image widget

--- lib/screans/Dashboard.dart
+++ lib/screans/Dashboard.dart
@@ -12,28 +12,31 @@ class Dashboard extends StatelessWidget {
         title: Text("Dashboard".toUpperCase()),
         backgroundColor: Colors.blueGrey,
       ),
-      body: Center(
-        // child: Text(
-        //   'hello world',
-        //   style: TextStyle(color: Colors.red, fontSize: 14),
-        // ),
-        // child: Text.rich(
-        //   TextSpan(
-        //     text: 'My',
-        //     children: [
-        //       TextSpan(
-        //           text: 'Flutter',
-        //           style:
-        //               TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold)),
-        //       TextSpan(
-        //           text: 'App',
-        //           style: TextStyle(fontSize: 30.0, color: Colors.blue)),
-        //     ],
-        //   ),
-        // ),
+      // body: Center(
+      //   // child: Text(
+      //   //   'hello world',
+      //   //   style: TextStyle(color: Colors.red, fontSize: 14),
+      //   // ),
+      //   // child: Text.rich(
+      //   //   TextSpan(
+      //   //     text: 'My',
+      //   //     children: [
+      //   //       TextSpan(
+      //   //           text: 'Flutter',
+      //   //           style:
+      //   //               TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold)),
+      //   //       TextSpan(
+      //   //           text: 'App',
+      //   //           style: TextStyle(fontSize: 30.0, color: Colors.blue)),
+      //   //     ],
+      //   //   ),
+      //   // ),
+      //
+      //   // child: Text("Random value is ${getNumber()}"),
+      // ),
 
-        child: Text("Random value is ${getNumber()}"),
-      ),
+      // body: Image(image: AssetImage("images/img.png"),),
+      body: Image(image: NetworkImage("https://www.linkpicture.com/q/gocheetalogo1.png"),),
     );
   }
 }
